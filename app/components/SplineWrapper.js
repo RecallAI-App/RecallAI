b58474c9a5a5b3b1016662aab161943983501dd4
DawnSaju <70035674+DawnSaju@users.noreply.github.com>
2024-08-30 22:37:11 +0400

    v3

--- app/components/SplineWrapper.js
+++ app/components/SplineWrapper.js
@@ -6,7 +6,7 @@ const SplineWrapper = () => {
   return (
     <div>
       <Spline
-        scene="https://prod.spline.design/Lgx8o2sXvioJAwOn/scene.splinecode" 
+        scene="https://prod.spline.design/4gGaIccJeoarwUbY/scene.splinecode" 
         style={{ width: '100%', height: '100%' }}
         className="lg:scale-100"
       />
